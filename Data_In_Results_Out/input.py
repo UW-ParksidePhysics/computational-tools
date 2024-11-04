1847ae3e80bcb0c68981063aa4a84cba158aa42e
connorrobertson01 <rober132@rangers.uwp.edu>
2024-11-04 12:01:41 -0600

    Updated HIV example 4.1

--- Data_In_Results_Out/input.py
+++ Data_In_Results_Out/input.py
@@ -4,4 +4,11 @@ import numpy as np
 def load_data():
     data_file = r'C:\Users\bleac\PycharmProjects\computational-tools\pmls-data\pmls-data\01HIVseries\HIVseries.csv'
     data_set = np.loadtxt(data_file, delimiter=',')
+
+    return data_set
+
+if __name__ == '__main__':
+    test_data_set = load_data()
+    print(test_data_set)
+
     #print(data_set)
